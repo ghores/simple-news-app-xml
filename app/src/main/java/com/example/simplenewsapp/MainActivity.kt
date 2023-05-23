68cc43ef2b03812e09e2dbaebf04be84db7ff338
ghazimoradi <ghazimoradi.amin@gmail.com>
2023-05-24 00:31:24 +0330

    implementation recyclerView

--- app/src/main/java/com/example/simplenewsapp/MainActivity.kt
+++ app/src/main/java/com/example/simplenewsapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.simplenewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val newsList = mutableListOf<News>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialRecView()
+    }
+
+    private fun initialRecView() {
+        val recView = findViewById<RecyclerView>(R.id.recView)
+        val newsAdapter = NewsAdaptor(newsList)
+        recView.adapter = newsAdapter
+        recView.layoutManager = LinearLayoutManager(this)
     }
 }
